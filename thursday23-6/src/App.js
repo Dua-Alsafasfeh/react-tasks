5fd49d52c53d7fa2cb99a6d5007aaf7bdc8476b0
Dua-Alsafasfeh <dua.alsafasfeh@gmail.com>
2022-06-23 15:38:22 -0700

    task

---
+++ thursday23-6/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Sortarr from './Component/Array.js' ;
+import Maping from './Component/Map';
+import Task3 from './Component/Color';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="container">
+      <Sortarr />
+      <Maping />
+      <Task3 />
+      </header>
+    </div>
+  );
+}
+export default App;
